0b3a52eaffbedd287b19905e7b663fc2854a4c06
Patryk Pawłowicz <patryk.pawlowicz@wp.pl>
2021-04-22 12:07:13 +0200

    basic layout v.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<header id="header">
+<img src="./assets/aMuse_logo.png" alt="aMuse">
+</header>
+<nav id="nav">
+  <ul>
+    <li class="li">
+      Home
+    </li>
+    <li class="li">
+      Log in
+    </li>
+    <li class="li">
+      Register
+    </li>
+  </ul>
+</nav>
+<main id="main">
+  <h3>Some lesson</h3>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam quod suscipit eaque voluptatem dolorem veniam, accusantium facere at sit minus nihil hic perferendis fugiat laudantium nam illum beatae est ipsam?</p>
+
+</main>
+<aside id="aside1">
+  Advertisement/Chat
+</aside>
+<footer id="footer">aMuse Team developer 2021</footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+#header{
+  justify-content: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
+}
+#nav{
+text-align: center;
+background-color:#A8B1BF;
+
+}
+#main{
+margin: 1%;
+width: 74%;
+float: left;
+}
+#aside1{
+width: 22%;
+margin: 1%;
+float: left
+}
+#footer{
+  padding: 10px;
+  text-align: center;
+  clear: both;
+
+}
+.li{
+  padding: 5px 5px 5px 5px;
+  margin: 5px;
+  display: inline-block;
+  background-color: #7dbed6;
+  border-radius: 25%;
 }
 </style>
